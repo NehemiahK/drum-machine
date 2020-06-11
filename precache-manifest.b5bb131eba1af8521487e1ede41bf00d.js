2b52a5dd10370a928e5858ff2278a9c613564616
nehemiahk <nehemiahkiv@gmail.com>
2020-06-11 15:47:55 +0300

    Updates

---
+++ precache-manifest.b5bb131eba1af8521487e1ede41bf00d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fcef23bf403cb9d034e70bcd7885f9b",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "e02d52e55bfef5cdd64c",
+    "url": "/drum-machine/static/css/main.9cad239f.chunk.css"
+  },
+  {
+    "revision": "f114b186699401542358",
+    "url": "/drum-machine/static/js/2.197444f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.197444f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e02d52e55bfef5cdd64c",
+    "url": "/drum-machine/static/js/main.c2240b10.chunk.js"
+  },
+  {
+    "revision": "fc1b3e84514a8c25bf54",
+    "url": "/drum-machine/static/js/runtime-main.1e07d996.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drum-machine/static/media/logo.5d5d9eef.svg"
+  }
+]);
